cab5cafeb15700392109e62dcabccf396775af90
anytodo92 <anything.todo92@gmail.com>
2022-12-20 18:25:03 +0900

    added some pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
-import CssBaseline from '@mui/material/CssBaseline';
 import { store } from './store';
-import theme from './styles/theme';
-
-import { ThemeProvider } from '@mui/material/styles';
-
-import { BrowserRouter } from 'react-router-dom';
 
 import './styles/base.css';
 import './index.css';
@@ -17,12 +11,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
+      <App />
     </Provider>
   </React.StrictMode>
 );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,15 +3,15 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  // Navigate,
-  // Outlet
+  Navigate,
+  Outlet,
 } from 'react-router-dom';
-// import Wrapper from 'layouts/Wrapper';
+import Wrapper from '../layout/Wrapper';
 
 // const PrivateRoute = lazy(() => import('PrivateRoute'));
 
 const Login = lazy(() => import('../views/auth/Login'));
-// const Dashboard = lazy(() => import('pages/Dashboard'));
+const Dashboard = lazy(() => import('../views/dashboard'));
 // const Posts = lazy(() => import('pages/Posts'));
 
 const AllRoutes = () => {
@@ -20,14 +20,14 @@ const AllRoutes = () => {
       <Suspense fallback={<h3>Loading...</h3>}>
         <Routes>
           <Route path='/login' element={<Login />} />
-          {/* <Route path="/" element={<Outlet />}>
-            <Route path="" element={<Wrapper />}>
-              <Route path="" element={<Navigate to="/dashboard" />} />
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="posts" element={<Posts />} />
-              <Route path="*" />
+          <Route path='/' element={<Outlet />}>
+            <Route path='' element={<Wrapper />}>
+              <Route path='' element={<Navigate to='/dashboard' />} />
+              <Route path='dashboard' element={<Dashboard />} />
+              {/*<Route path="posts" element={<Posts />} />*/}
+              {/*<Route path="*" />*/}
             </Route>
-          </Route> */}
+          </Route>
         </Routes>
       </Suspense>
     </Router>
